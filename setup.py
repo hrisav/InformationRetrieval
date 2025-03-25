ba5b54ace4e61e764e210a833dec49f8f7e4ddae
Hrisav Bhowmick <knightmusicriders@gmail.com>
2025-03-25 22:22:12 +0530

    requirements, setup  added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Document Retrieval Project',
+    version= '0.0.0',
+    author= 'Hrisav',
+    author_email= 'knightmusicriders@gmail.com',
+    packages= find_packages(),            # help to install "src" (folder where __init__.py is present) as local package
+    install_requires = []
+)
